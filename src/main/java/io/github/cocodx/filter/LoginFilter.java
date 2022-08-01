774f66df3d6ccb3955c74366ff461b318e00fd3b
刘刚 <2cevening@gmail.com>
2022-08-02 00:06:27 +0800

    feature(diary):登录过滤器
    没有做好，待完善

---
+++ src/main/java/io/github/cocodx/filter/LoginFilter.java
@@ -0,0 +1,37 @@
+package io.github.cocodx.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author amazfit
+ * @date 2022-08-01 下午11:55
+ **/
+public class LoginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession();
+        Object currentUser = session.getAttribute("currentUser");
+        String servletPath = request.getServletPath();
+        if(currentUser==null && !servletPath.contains("login") && !servletPath.contains("bootstrap") && !servletPath.contains("images")){
+            response.sendRedirect("login.jsp");
+        }else{
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
